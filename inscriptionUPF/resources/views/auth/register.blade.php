c06080c67d57120ff2f11442ec267f6471289df1
elakrouche <elakrouchemohammed@gmail.com>
2022-01-07 20:07:56 +0100

    design

--- inscriptionUPF/resources/views/auth/register.blade.php
+++ inscriptionUPF/resources/views/auth/register.blade.php
@@ -1,21 +1,58 @@
-@extends('layouts.app')
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+<!DOCTYPE html>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Universite Prive De Fes</title>
+	<link rel="icon" type="image/png" href= "{{ url('image/images.jpg') }}">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="{{ url('css/formLogin.css') }}">
+	
+	<link rel="stylesheet" type="text/css" href="{{ url('css/barre.css') }}">
+	
+</head>
+<body>
+	
+<div class="hero">
+		<nav>
+			<img class="logo" src="{{ url('image/images.jpg') }}">
+			<ul>
+				<li><a href="#">Bienvenue sur notre site d'inscription</a></li>
+				
+			</ul>
+			<a href="{{ route('login') }}" class="xtn">Adminstration</a>
+			<a href="{{ route('login') }}" class="xtn">Deja Condidat</a>
+			<a href="{{ route('register') }}" class="xtn">New condidat</a>
+		</nav>
+
+		<div class="content">
+			
+			</div>
+		</div>
+	</div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 offset-md-4 form login-form">
+   
+      
+            <form method="POST" action="{{ route('register') }}">
+            <h2 class="text-center"><img src="{{ url('image/logo_upf.png') }}"></h2>
+					
+                    <p class="text-center">Universite Prive De Fes</p>
+                   
+                   
+                        <?php
+                    
+                    ?>
+                     @csrf
                         @csrf
 
-                        <div class="row mb-3">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
+                        <div class="form-group">
+                           
 
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                            
+                                <input id="name" placeholder="name" type="text" class="form-control " name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
 
                                 @error('name')
                                     <span class="invalid-feedback" role="alert">
@@ -23,13 +60,11 @@
                                     </span>
                                 @enderror
                             </div>
-                        </div>
+                        
 
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+                        <div class="form-group">
+                           
+                                <input id="email" placeholder="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
 
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
@@ -37,13 +72,11 @@
                                     </span>
                                 @enderror
                             </div>
-                        </div>
+                        
 
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+                        <div class="form-group">
+                            
+                                <input id="password" type="password" placeholder="enter votre password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
 
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
@@ -51,27 +84,33 @@
                                     </span>
                                 @enderror
                             </div>
+                        
+
+                        <div class="form-group">
+                           
+                                <input id="password-confirm" placeholder=" confirm your password" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                            </div>
+                            <div class="form-group">
+        <button  class="form-control button" type="submit" class="btn btn-primary">
+        {{ __('Register') }}
+        </button>
+
+       
+    </div>
                         </div>
 
-                        <div class="row mb-3">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
+                      
                         </div>
                     </form>
-                </div>
+  
+    
+   
             </div>
         </div>
     </div>
-</div>
-@endsection
+    </div>
+        </div>
+    </div>
+    
+</body>
+</html>
